8f3c0cabbf28e093eaffb59b109fdf21f4cd3936
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-08-12 15:05:50 +0530

    Create new articles through front end and display flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article is created successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
 end
