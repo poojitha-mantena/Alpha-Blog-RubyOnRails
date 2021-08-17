273d6b849378d08ba737ca1b7f425501106e9999
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-08-17 23:38:27 +0530

    Add sessions create and destroy for logging users in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,22 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def created
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There is something wrong with your login details"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
   end
 
 end
