80054f8882fbd8099ad0bbb023b49c7d2a64f08d
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-08-18 19:09:51 +0530

    Add auth methods to controller as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article is created successfully"
       redirect_to article_path(@article)
